d7981cea66d11d55c18f784aafd38970864af24c
Mohamed magdy <mohammedmego15@gmail.com>
2024-09-12 12:02:08 +0300

    done Auth Featuers UI

---
+++ lib/features/auth/presentation/views/screens/login_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:selaty_app/core/routers/routers_manager.dart';
+import 'package:selaty_app/core/utils/Strings_app.dart';
+import 'package:selaty_app/core/utils/colors.dart';
+import 'package:selaty_app/core/utils/functions.dart';
+import 'package:selaty_app/core/utils/resposive.dart';
+import 'package:selaty_app/core/utils/text_styles.dart';
+import 'package:selaty_app/core/widgets/custom_button.dart';
+import 'package:selaty_app/features/auth/presentation/views/widgets/custom_appbar.dart';
+import 'package:selaty_app/features/auth/presentation/views/widgets/login/section_text_field_login_screen.dart';
+import 'package:selaty_app/features/auth/presentation/views/widgets/section_name_auth_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppbarAuth(
+        iconRight: CupertinoIcons.camera_on_rectangle,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: context.width * 0.055),
+        child: OrientationBuilder(builder: (context, orientation) {
+          if (orientation == Orientation.landscape) {
+            return Center(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: context.width * 0.8,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: context.width * 0.8,
+                            child: const SectionTextFeildLoginScreen(),
+                          ),
+                          SizedBox(height: context.height * 0.002),
+                          TextButton(
+                            onPressed: () {
+                              GoRouter.of(context)
+                                  .push(RoutersManager.kChangePasswordScreen);
+                            },
+                            child: Text(
+                              StringsApp.forgetPassword,
+                              style: StylesManager.textStyle_8_Medium(context)
+                                  .copyWith(
+                                color: primaryDarkGrey,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: context.height * 0.02),
+                          CustomButton(
+                            colorButton: primaryGreen,
+                            titleButton: StringsApp.loginAccount,
+                            onTap: () {
+                              GoRouter.of(context)
+                                  .pushReplacement(RoutersManager.kHomeScreen);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SectionNameAuthScreen(),
+                  ],
+                ),
+              ),
+            );
+          }
+          return SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                isKeybordVisible(context)
+                    ? const SizedBox(height: 0)
+                    : SizedBox(height: context.height * 0.04),
+                isKeybordVisible(context)
+                    ? const SizedBox(height: 0)
+                    : const SectionNameAuthScreen(),
+                SizedBox(height: context.height * 0.04),
+                const SectionTextFeildLoginScreen(),
+                SizedBox(height: context.height * 0.015),
+                TextButton(
+                  onPressed: () {
+                    GoRouter.of(context)
+                        .push(RoutersManager.kChangePasswordScreen);
+                  },
+                  child: Text(
+                    StringsApp.forgetPassword,
+                    style: StylesManager.textStyle_8_Medium(context).copyWith(
+                      color: primaryDarkGrey,
+                    ),
+                  ),
+                ),
+                SizedBox(height: context.height * 0.035),
+                CustomButton(
+                  colorButton: primaryGreen,
+                  titleButton: StringsApp.loginAccount,
+                  onTap: () {
+                    GoRouter.of(context)
+                        .pushReplacement(RoutersManager.kHomeScreen);
+                  },
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/presentation/views/widgets/custom_appbar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:selaty_app/core/utils/colors.dart';
+import 'package:selaty_app/core/utils/functions.dart';
+import 'package:selaty_app/core/utils/resposive.dart';
+
+class CustomAppbarAuth extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppbarAuth({super.key, required this.iconRight});
+  final IconData iconRight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: context.height * 0.045,
+        bottom: context.height * 0.01,
+        right: context.width * 0.05,
+        left: context.width * 0.05,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: context.height * 0.045,
+              width: context.height * 0.045,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: primaryLightGrey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Icon(
+                iconRight,
+                color: primaryDarkGrey,
+                size: 16,
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              hideKeybourd();
+              Navigator.pop(context);
+            },
+            child: Container(
+              height: context.height * 0.045,
+              width: context.height * 0.045,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: primaryLightGrey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Icon(
+                  CupertinoIcons.back,
+                  textDirection: TextDirection.ltr,
+                  color: primaryDarkGrey,
+                  size: 20,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(90);
+}
