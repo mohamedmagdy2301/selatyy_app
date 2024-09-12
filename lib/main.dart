99e20d80e01332d7f880b06e928e33cf516a83e0
Mohamed magdy <mohammedmego15@gmail.com>
2024-09-12 10:59:43 +0300

    Add assets and packages in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:selatyy_app/selaty_app.dart';
+import 'package:selaty_app/selaty_app.dart';
 
 void main() {
   runApp(const SelatyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:selatyy_app/selaty_app.dart';
+import 'package:selaty_app/selaty_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
