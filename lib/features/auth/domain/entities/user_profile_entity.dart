94ecd3785df8bb1b9103223b994533b402156945
Mohamed magdy <mohammedmego15@gmail.com>
2024-10-22 15:54:47 +0300

    added user profile entity

---
+++ lib/features/auth/domain/entities/user_profile_entity.dart
@@ -0,0 +1,13 @@
+class UserProfileEntity {
+  final String name;
+  final String email;
+  final String phone;
+  final String image;
+
+  UserProfileEntity({
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.image,
+  });
+}
