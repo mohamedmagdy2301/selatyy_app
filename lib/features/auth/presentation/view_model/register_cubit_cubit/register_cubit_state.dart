98e1d6caf063b8134067655c25c88bc1ac5607bc
Mohamed magdy <mohammedmego15@gmail.com>
2024-10-24 03:00:08 +0300

    create RegisterCubitState

---
+++ lib/features/auth/presentation/view_model/register_cubit_cubit/register_cubit_state.dart
@@ -0,0 +1,13 @@
+part of 'register_cubit_cubit.dart';
+
+class RegisterCubitState {}
+
+final class RegisterCubitInitial extends RegisterCubitState {}
+
+final class RegisterCubitLoading extends RegisterCubitState {}
+
+final class RegisterCubitRequired extends RegisterCubitState {}
+
+final class RegisterCubitSuccess extends RegisterCubitState {}
+
+final class RegisterCubitFailure extends RegisterCubitState {}
