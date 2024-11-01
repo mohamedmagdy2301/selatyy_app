971000ae93c7a4283b518a581806428c86f919da
Mohamed magdy <mohammedmego15@gmail.com>
2024-11-01 11:23:16 +0200

    a

--- lib/selaty app/selaty_app.dart
+++ lib/selaty app/selaty_app.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:selaty/core/routers/routers_manager.dart';
 import 'package:selaty/features/auth/presentation/view_model/update_user_profile_cubit/update_user_profile_cubit.dart';
-import 'package:selaty/features/home/presentation/view%20model/add_favorite_product_cubit/add_favorite_product_cubit.dart';
+import 'package:selaty/features/home/presentation/view%20model/favorite_product_cubit/favorite_product_cubit.dart';
 import 'package:selaty/features/home/presentation/view%20model/categories_cubit/categories_cubit.dart';
 import 'package:selaty/features/home/presentation/view%20model/products_cubit/products_cubit.dart';
 import 'package:selaty/features/home/presentation/view%20model/slider_cubit/slider_cubit.dart';
@@ -20,7 +20,7 @@ class SelatyApp extends StatelessWidget {
         child: BlocProvider(
           create: (context) => ProductsCubit()..getProducts(),
           child: BlocProvider(
-            create: (context) => AddFavoriteProductCubit(),
+            create: (context) => FavoriteProductCubit(),
             child: BlocProvider<UpdateUserProfileCubit>(
               create: (context) => UpdateUserProfileCubit(),
               child: MaterialApp.router(
