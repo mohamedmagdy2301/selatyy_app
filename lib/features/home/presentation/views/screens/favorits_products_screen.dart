cf4032df58910ca42db12b84e26207a3eb5cb95c
Mohamed magdy <mohammedmego15@gmail.com>
2024-11-03 11:37:14 +0200

    change favorites to responsive landscape

--- lib/features/home/presentation/views/screens/favorits_products_screen.dart
+++ lib/features/home/presentation/views/screens/favorits_products_screen.dart
@@ -4,24 +4,58 @@ import 'package:selaty/core/utils/resposive.dart';
 import 'package:selaty/core/utils/text_styles.dart';
 import 'package:selaty/features/home/presentation/views/widgets/build/build_favorite.dart';
 
-class FavoritsProductsScreen extends StatelessWidget {
-  const FavoritsProductsScreen({super.key});
+class FavoritesProductsScreen extends StatelessWidget {
+  const FavoritesProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backGroundScaffold,
-      appBar: AppBar(
-        toolbarHeight: context.height * .07,
-        title: Text(
-          'سلة المفضلات',
-          style: StylesManager.textStyle_28_bold(context).copyWith(
-            color: primaryBlack,
-          ),
-        ),
-        flexibleSpace: Container(color: backGroundScaffold),
-        centerTitle: true,
+      appBar: _buildAppBar(context, context.isLandscape),
+      body: SafeArea(
+        bottom: !context.isLandscape,
+        child: context.isLandscape ? _buildLandscapeBody() : BuildFavorite(),
       ),
+    );
+  }
+
+  PreferredSizeWidget? _buildAppBar(BuildContext context, bool isLandscape) {
+    return isLandscape
+        ? null
+        : AppBar(
+            toolbarHeight: context.height * 0.07,
+            title: Text(
+              'سلة المفضلات',
+              style: StylesManager.textStyle_28_bold(context).copyWith(
+                color: primaryBlack,
+              ),
+            ),
+            centerTitle: true,
+            flexibleSpace: Container(color: backGroundScaffold),
+          );
+  }
+
+  Widget _buildLandscapeBody() {
+    return NestedScrollView(
+      physics: BouncingScrollPhysics(),
+      headerSliverBuilder: (context, innerBoxIsScrolled) {
+        return [
+          SliverAppBar(
+            pinned: false,
+            floating: true,
+            snap: true,
+            toolbarHeight: context.height * 0.07,
+            title: Text(
+              'سلة المفضلات',
+              style: StylesManager.textStyle_28_bold(context).copyWith(
+                color: primaryBlack,
+              ),
+            ),
+            centerTitle: true,
+            flexibleSpace: Container(color: backGroundScaffold),
+          ),
+        ];
+      },
       body: BuildFavorite(),
     );
   }
--- lib/selaty app/main_scaffold.dart
+++ lib/selaty app/main_scaffold.dart
@@ -48,7 +48,7 @@ class MainScaffold extends StatelessWidget {
     return [
       HomeScreen(),
       SearchScreen(),
-      FavoritsProductsScreen(),
+      FavoritesProductsScreen(),
       ProfileScreen(),
     ];
   }
