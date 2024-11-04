106967dec253109ae2711059dc4597c3512b10f8
Mohamed magdy <mohammedmego15@gmail.com>
2024-11-04 12:37:00 +0200

    change profile screen to responsive landscape

--- lib/features/auth/presentation/views/screens/profile_screen.dart
+++ lib/features/auth/presentation/views/screens/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:selaty/core/utils/colors.dart';
 import 'package:selaty/core/utils/resposive.dart';
+import 'package:selaty/core/utils/text_styles.dart';
 import 'package:selaty/features/auth/presentation/views/widgets/profile/custom_appbar.dart';
 import 'package:selaty/features/auth/presentation/views/widgets/profile/section_log_and_help.dart';
 import 'package:selaty/features/auth/presentation/views/widgets/profile/section_your_picture.dart';
@@ -20,26 +22,52 @@ class ProfileScreen extends StatelessWidget {
         child: CustomScrollView(
           slivers: [
             SliverAppBar(
-              pinned: true,
-              expandedHeight: context.height * 0.08,
+              pinned: !context.isLandscape,
+              floating: context.isLandscape,
+              snap: context.isLandscape,
+              elevation: 0,
+              // expandedHeight: context.height * 0.08,
               toolbarHeight: context.height * 0.075,
-              flexibleSpace: Container(
-                color: Color.fromARGB(255, 238, 238, 238),
+              title: Text(
+                'الملف الشخصي',
+                style: StylesManager.textStyle_28_bold(context)
+                    .copyWith(color: primaryBlack),
               ),
-              actions: CustomAppbarProfile.appBarActions(context, () {
-                tabCubit.updateTabIndex(2);
-              }),
-              automaticallyImplyLeading: false,
+              centerTitle: true,
+              flexibleSpace: Container(color: backGroundScaffold),
+              leading: CustomAppbarProfile.appBarActions(
+                  context, () => tabCubit.updateTabIndex(2)),
             ),
-            SliverToBoxAdapter(child: SizedBox(height: context.height * 0.07)),
-            SliverToBoxAdapter(child: SectionYourPicture()),
-            SliverToBoxAdapter(child: SizedBox(height: context.height * 0.018)),
-            // SliverToBoxAdapter(child: SectionInfoAboutMe()),
-            SliverToBoxAdapter(child: SectionLogAndHelp()),
-            SliverToBoxAdapter(child: SizedBox(height: context.height * 0.018)),
+            context.isLandscape
+                ? SliverToBoxAdapter(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SectionLogAndHelp(),
+                        SectionYourPicture(),
+                      ],
+                    ),
+                  )
+                : SliverToBoxAdapter(
+                    child: Column(
+                      children: [
+                        SizedBox(height: context.height * 0.06),
+                        SectionYourPicture(),
+                        SizedBox(height: context.height * 0.018),
+                        SectionLogAndHelp(),
+                        SizedBox(height: context.height * 0.018),
+                      ],
+                    ),
+                  ),
           ],
         ),
       ),
     );
   }
 }
+            // SliverToBoxAdapter(child: SizedBox(height: context.height * 0.07)),
+            // SliverToBoxAdapter(child: SectionYourPicture()),
+            // SliverToBoxAdapter(child: SizedBox(height: context.height * 0.018)),
+            // SliverToBoxAdapter(child: SectionLogAndHelp()),
+            // SliverToBoxAdapter(child: SizedBox(height: context.height * 0.018)),
--- lib/features/auth/presentation/views/widgets/profile/custom_appbar.dart
+++ lib/features/auth/presentation/views/widgets/profile/custom_appbar.dart
@@ -4,34 +4,17 @@ import 'package:selaty/core/utils/functions.dart';
 import 'package:selaty/core/utils/resposive.dart';
 
 class CustomAppbarProfile {
-  static List<Widget> appBarActions(BuildContext context, Function()? onTap) {
-    return [
-      SizedBox(width: context.width * 0.02),
-      Spacer(),
-      GestureDetector(
-        onTap: () {
-          hideKeybourd();
-          onTap!();
-        },
-        child: Container(
-          height: context.height * 0.045,
-          width: context.height * 0.045,
-          decoration: BoxDecoration(
-            color: primaryWhite,
-            border: Border.all(color: primaryLightGrey),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: const Center(
-            child: Icon(
-              CupertinoIcons.back,
-              textDirection: TextDirection.ltr,
-              color: primaryDarkGrey,
-              size: 20,
-            ),
-          ),
-        ),
+  static Widget appBarActions(BuildContext context, Function()? onTap) {
+    return GestureDetector(
+      onTap: () {
+        hideKeybourd();
+        onTap!();
+      },
+      child: Icon(
+        CupertinoIcons.arrow_right,
+        color: primaryDarkGrey,
+        size: context.textScale * 7,
       ),
-      SizedBox(width: context.width * 0.04),
-    ];
+    );
   }
 }
